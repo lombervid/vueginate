2905c66187ba5162015f7deec04c68a6e563a2ec
David Lombera <david.lomberlan@gmail.com>
2023-04-17 19:50:02 -0600

    Initial commit

---
+++ example/src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Vueginate example</h1>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'Vueginate',
+      fileName: 'vueginate'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
