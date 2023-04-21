2307a6f1f8e17ab2699b428d2a1352175990cf8e
David Lombera <david.lomberlan@gmail.com>
2023-04-21 13:03:57 -0600

    add plain templates

---
+++ src/components/VueginateCore.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import PreviousIcon from './Icons/PreviousIcon.vue'
+import NextIcon from './Icons/NextIcon.vue'
+
+type HTMLEntitiesKey = keyof typeof HTMLEntities
+
+const HTMLEntities = {
+  ellipsis: {
+    unicode: '&#8230;',
+    alphanumeric: '&hellip;'
+  },
+  lquot: {
+    unicode: '&#171;',
+    alphanumeric: '&laquo;'
+  },
+  rquot: {
+    unicode: '&#187;',
+    alphanumeric: '&raquo;'
+  }
+}
+
+function htmlEntity(name: HTMLEntitiesKey, unicode: boolean = false): string {
+  let character = HTMLEntities[name] ?? null
+
+  if (!character) {
+    return ''
+  }
+
+  return unicode ? character.unicode : character.alphanumeric
+}
+</script>
+<template>
+  <nav aria-label="Page navigation">
+    <ul class="pagination">
+      <li>
+        <a href="#" class="page-item arrow">
+          <span class="sr-only">Prev Page</span>
+          <PreviousIcon />
+        </a>
+      </li>
+      <li>
+        <a href="#" class="page-item"> 1 </a>
+      </li>
+      <li>
+        <span class="page-item disabled" v-html="htmlEntity('ellipsis', true)"></span>
+      </li>
+      <li>
+        <a href="#" class="page-item"> 13 </a>
+      </li>
+      <li>
+        <a href="#" class="page-item"> 14 </a>
+      </li>
+      <li class="page-item active">15</li>
+      <li>
+        <a href="#" class="page-item"> 16 </a>
+      </li>
+      <li>
+        <a href="#" class="page-item"> 17 </a>
+      </li>
+      <li>
+        <span class="page-item disabled"> ... </span>
+      </li>
+      <li>
+        <a href="#" class="page-item"> 30 </a>
+      </li>
+      <li>
+        <a href="#" class="page-item arrow">
+          <span class="sr-only">Next Page</span>
+          <NextIcon />
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+/* @tailwind base; */
+/* @tailwind utilities; */
+
+.pagination {
+  @apply flex select-none justify-center gap-1 text-xs font-medium text-gray-600;
+}
+
+.pagination .page-item {
+  @apply block h-8 w-8 rounded border border-gray-300 text-center leading-8;
+}
+.pagination .page-item:hover:not(.active):not(.disabled) {
+  @apply border-gray-400 bg-gray-50;
+}
+.pagination .page-item.active {
+  @apply border-blue-500 bg-blue-50 text-blue-600;
+  /* @apply border-violet-500 bg-violet-50 text-violet-600; */
+}
+.pagination .page-item.disabled {
+  @apply bg-gray-200 opacity-50;
+}
+.pagination .page-item.arrow {
+  @apply inline-flex  items-center justify-center p-2;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-export {}
+import VueginateBootstrap from './components/VueginateBootstrap.vue'
+import VueginateBulma from './components/VueginateBulma.vue'
+import VueginateCore from './components/VueginateCore.vue'
+import VueginateTailwind from './components/VueginateTailwind.vue'
+
+export { VueginateCore, VueginateBootstrap, VueginateBulma, VueginateTailwind }
