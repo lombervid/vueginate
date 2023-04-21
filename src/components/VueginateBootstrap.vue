048e9369b53ca6faa9950dc66a8d87dbfe1dc417
David Lombera <david.lomberlan@gmail.com>
2023-04-21 13:03:57 -0600

    feat: add plain templates

---
+++ src/components/VueginateBootstrap.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!-- Bootstrap -->
+  <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item disabled">
+        <a class="page-link">&hellip;</a>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">8</a></li>
+      <li class="page-item"><a class="page-link" href="#">9</a></li>
+      <li class="page-item active" aria-current="page">
+        <span class="page-link">10</span>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">11</a></li>
+      <li class="page-item"><a class="page-link" href="#">12</a></li>
+      <li class="page-item disabled">
+        <span class="page-link">...</span>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">20</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ src/components/VueginateTailwind.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import PreviousIcon from './Icons/PreviousIcon.vue'
+import NextIcon from './Icons/NextIcon.vue'
+</script>
+
+<template>
+  <!-- Tailwind -->
+  <nav aria-label="Page navigation">
+    <ul class="flex select-none justify-center gap-1 text-xs font-medium text-gray-600">
+      <li>
+        <a
+          href="#"
+          class="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-300 p-2 text-center leading-8 hover:border-gray-400 hover:bg-gray-50"
+        >
+          <span class="sr-only">Prev Page</span>
+          <PreviousIcon />
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block h-8 w-8 rounded border border-gray-300 text-center leading-8 hover:border-gray-400 hover:bg-gray-50"
+        >
+          1
+        </a>
+      </li>
+      <li>
+        <span
+          class="block h-8 w-8 rounded border border-gray-300 bg-gray-200 text-center leading-8 opacity-50"
+        >
+          ...
+        </span>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block h-8 w-8 rounded border border-gray-300 text-center leading-8 hover:border-gray-400 hover:bg-gray-50"
+        >
+          13
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block h-8 w-8 rounded border border-gray-300 text-center leading-8 hover:border-gray-400 hover:bg-gray-50"
+        >
+          14
+        </a>
+      </li>
+      <li
+        class="block h-8 w-8 rounded border border-blue-500 bg-blue-50 text-center leading-8 text-blue-600"
+      >
+        15
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block h-8 w-8 rounded border border-gray-300 text-center leading-8 hover:border-gray-400 hover:bg-gray-50"
+        >
+          16
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block h-8 w-8 rounded border border-gray-300 text-center leading-8 hover:border-gray-400 hover:bg-gray-50"
+        >
+          17
+        </a>
+      </li>
+      <li>
+        <span
+          class="block h-8 w-8 rounded border border-gray-300 bg-gray-200 text-center leading-8 opacity-50"
+        >
+          ...
+        </span>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block h-8 w-8 rounded border border-gray-300 text-center leading-8 hover:border-gray-400 hover:bg-gray-50"
+        >
+          30
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-300 p-2 text-center leading-8 hover:border-gray-400 hover:bg-gray-50"
+        >
+          <span class="sr-only">Next Page</span>
+          <NextIcon />
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
