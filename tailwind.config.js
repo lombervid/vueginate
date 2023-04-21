0d12d51bf98b8fb90ba76e8d74d714b746d0837b
David Lombera <david.lomberlan@gmail.com>
2023-04-21 13:03:26 -0600

    build: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./example/index.html', './example/src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
